5d7314711bfedd1d29d95c2f0f26f1add1643eaf
ntheanh <tozaco51@gmail.com>
2024-09-30 10:13:26 +0700

    feat: Design list, chat, details message

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,147 @@
+import EmojiPicker from "emoji-picker-react"
+import { useState, useRef, useEffect } from "react"
 
 const Chat = () => {
+  const [openEmoji, setOpenEmoji] = useState(false)
+  const [text, setText] = useState("")
+  const endRef = useRef(null)
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [])
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji)
+    // setOpenEmoji(false)
+  }
   return (
-    <div className="flex-[2]">
-      Chat
+    <div className="flex-[2] border-r border-l flex flex-col ">
+      {/*Top */}
+      <div className="flex items-center justify-between border-b pb-2 p-4">
+        <div className="flex items-center gap-4">
+          <img
+            src="./avatar.png"
+            alt=""
+            className="w-12 h-12 rounded-full object-cover "
+          />
+          <div>
+            <h2 className="text-base font-semibold">Maria Nelson</h2>
+            <span className="text-sm">Lorem ipsum dolor sit amet.</span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <img src="./phone.png" alt="" className="w-5 h-5 cursor-pointer " />
+          <img src="./video.png" alt="" className="w-5 h-5 cursor-pointer " />
+          <img src="./info.png" alt="" className="w-5 h-5 cursor-pointer " />
+        </div>
+      </div>
+      {/*Middle */}
+      <div className="flex-[1] flex flex-col p-4 gap-5 overflow-scroll no-scrollbar">
+        <div className=" flex gap-4 max-w-[70%] ">
+          <img src="./avatar.png" alt="" className="w-8 h-8 rounded-full" />
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue_blur p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>{" "}
+        <div className=" flex gap-4 max-w-[70%] own">
+          <div className="flex flex-col gap-2">
+            <img
+              src="./avatar.png"
+              alt=""
+              className="max-w-[70%] object-cover rounded-lg "
+            />
+            <span className="bg-blue p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>{" "}
+        <div className=" flex gap-4 max-w-[70%] ">
+          <img src="./avatar.png" alt="" className="w-8 h-8 rounded-full" />
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue_blur p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>{" "}
+        <div className=" flex gap-4 max-w-[70%] own">
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>{" "}
+        <div className=" flex gap-4 max-w-[70%] ">
+          <img src="./avatar.png" alt="" className="w-8 h-8 rounded-full" />
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue_blur p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>
+        <div className=" flex gap-4 max-w-[70%] own">
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>{" "}
+        <div className=" flex gap-4 max-w-[70%] ">
+          <img src="./avatar.png" alt="" className="w-8 h-8 rounded-full" />
+          <div className="flex flex-col gap-2">
+            <span className="bg-blue_blur p-2 rounded-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+              inventore?
+            </span>
+            <span className="text-xs font-light">10 minutes ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
+      {/* Bottom */}
+      <div className="flex items-center justify-between p-4 mt-auto border-t">
+        <div className="flex items-center gap-3">
+          <img src="./img.png" alt="" className="w-5 h-5 cursor-pointer" />
+          <img src="./camera.png" alt="" className="w-5 h-5 cursor-pointer" />
+          <img src="./mic.png" alt="" className="w-5 h-5 cursor-pointer" />
+        </div>
+        <div className="flex-[2] mx-4">
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="bg-blue_blur border-none outline-none rounded p-2 w-full text-sm"
+          />
+        </div>
+        <div className="flex items-center gap-3 ">
+          <img
+            src="./emoji.png"
+            alt=""
+            className="w-5 h-5 cursor-pointer relative"
+            onClick={() => setOpenEmoji((prev) => !prev)}
+          />
+          <div className="absolute bottom-12 right-0">
+            <EmojiPicker open={openEmoji} onEmojiClick={handleEmoji} />
+          </div>
+          <button className="border-none outline-none p-2 bg-purple rounded">
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/List/chatList/ChatList.jsx
+++ src/components/List/chatList/ChatList.jsx
@@ -3,13 +3,112 @@ import { useState } from "react"
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false)
   return (
-    <div className="p-4">
+    <div className="p-4 flex-[1] overflow-y-scroll max-h-[calc(100vh-20vh)] no-scrollbar">
       <div className="flex items-center gap-4 ">
         <div className="flex items-center gap-2 bg-blue_blur p-2 rounded-lg flex-[1]">
           <img src="/search.png" alt="" className="w-4 h-4" />
-          <input type="text" placeholder="Search" className=" outline-none border-none bg-transparent" />
+          <input
+            type="text"
+            placeholder="Search"
+            className=" outline-none border-none bg-transparent"
+          />
         </div>
-        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="bg-blue_blur w-8 h-8 p-2 rounded-lg cursor-pointer" onClick={() => setAddMode(prev => !prev)} />
+        <div className="bg-blue_blur p-3 rounded-lg cursor-pointer">
+          <img
+            src={addMode ? "./minus.png" : "./plus.png"}
+            alt=""
+            className=" w-4 h-4"
+            onClick={() => setAddMode((prev) => !prev)}
+          />
+        </div>
+      </div>
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddddddf6] border-b pb-4 ">
+        <img
+          src="./avatar.png"
+          alt=""
+          className="w-12 h-12 object-cover rounded-full"
+        />
+        <div className="flex flex-col gap-2 ">
+          <h2 className="font-semibold">John smith</h2>
+          <span className="text-sm text-gray-500">good luck</span>
+        </div>
+      </div>
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
+      </div>{" "}
+      <div className="flex items-center gap-4 mt-4 border-solid border-[#ddd] border-b pb-2 ">
+        <img src="./avatar.png" alt="" className="w-10 h-10 rounded-full" />
+        <div>
+          <h2>John smith</h2>
+          <span>good luck</span>
+        </div>
       </div>
     </div>
   )
